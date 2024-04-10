736b205f064c49cc6fece3f6d7400dfc848dab44
davitidaviti123 <davit.meparishvili111@gmail.com>
2024-04-10 20:04:23 +0400

    second commit

---
+++ Group 05 homeworks/group 05 day 52/homework/work.js
@@ -0,0 +1,5 @@
+function infoJoin(name,lastName,age,home,hobby){
+    const combine = name+lastName+age+home+hobby
+    return combine
+}
+infoJoin("davit","meparishvili",12,"Tbilisi (didi dighomi)","coding")
---
+++ Group 05 homeworks/group 05 day 52/homework/work2.js
@@ -0,0 +1,11 @@
+function ageCheck(age){
+    if (age > 18) {
+        console.log("metia")
+     }
+     else {
+        console.log("naklebia")
+     }
+     if (age == 18) {
+        console.log("tolia")
+     }
+}

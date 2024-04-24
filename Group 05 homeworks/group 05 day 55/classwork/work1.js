a9a69bdc35597922a32d371be57e77bfff5e44dd
davitidaviti123 <davit.meparishvili111@gmail.com>
2024-04-24 19:03:04 +0400

    fifth commit

---
+++ Group 05 homeworks/group 05 day 55/classwork/work1.js
@@ -0,0 +1,6 @@
+function mathematicThings(num1){      // creating function with num1 variable
+    console.log(num1 * 2)
+    console.log(num1/2)                  //function content
+    console.log(num1*num1)
+}
+mathematicThings(5)            // summoning function

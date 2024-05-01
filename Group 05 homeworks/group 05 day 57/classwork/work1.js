ab7f9936a66538c837cca1c6c7afebd920eaecd3
davitidaviti123 <davit.meparishvili111@gmail.com>
2024-05-01 18:00:32 +0400

    seventh-commit

---
+++ Group 05 homeworks/group 05 day 57/classwork/work1.js
@@ -0,0 +1,15 @@
+const me = {
+    name: "david",
+    email: "davit.meparishvili111@gmail.com",
+    age: 12,
+    work: {
+        money: "1000$",
+        workTime: "12hr"
+    }
+}
+console.log(me.name , me.email , me.age , me.work.money , me.work.workTime)
+me.name = "giorgi"
+me.email = "giorgi.meparishvili2011@gmail.com"
+me.age = 13
+me.work.money = "10000$"
+me.work.workTime = "24hr"
---
+++ Group 05 homeworks/group 05 day 57/homework/work2.js
@@ -0,0 +1,15 @@
+const me = {
+    PC: {
+        cpu: "i7-intel",
+        gpu: "intel graphics 620"
+    },
+    work: {
+        time: "2hr",
+        money: "1000$"
+    }
+}
+me.PC.cpu = "i9-intel"
+me.PC.gpu = "NVIDIA graphics 3060 RTX"
+me.work.time = "10hr"
+me.work.money = "10000$"
+console.log(me)

9a7490e7ab0c2a73ee007bd1ac9c318e093aea25
davitidaviti123 <davit.meparishvili111@gmail.com>
2024-11-01 16:43:17 +0400

    next commit

--- Group 06 homeworks/group 06 day 93/homework/work1.js
+++ Group 06 homeworks/group 06 day 93/homework/work1.js
@@ -1,1 +1,35 @@
-//nothing here yet...
+window.onload = function() {
+    let posX = 0; 
+    let posY = 0;
+    let direction = "right";
+    let box = document.getElementById('box');
+    let t = setInterval(move, 10);
+
+    function move() {
+        if (direction === "right") {
+            posX += 1;
+            box.style.left = posX + 'px';
+            if (posX >= 150) {
+                direction = "down";
+            }
+        } else if (direction === "down") {
+            posY += 1;
+            box.style.top = posY + 'px';
+            if (posY >= 50) {
+                direction = "left";
+            }
+        } else if (direction === "left") {
+            posX -= 1;
+            box.style.left = posX + 'px';
+            if (posX <= 0) {
+                direction = "up";
+            }
+        } else if (direction === "up") {
+            posY -= 1;
+            box.style.top = posY + 'px';
+            if (posY <= 0) {
+                direction = "right";
+            }
+        }
+    }
+};

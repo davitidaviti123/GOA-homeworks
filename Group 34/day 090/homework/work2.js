32a94466e30f47eee06fd6237fbc2d0882df2706
unknown <davitidaviti>
2025-02-02 12:06:23 +0400

    next commit

--- Group 34/day 090/homework/work2.js
+++ Group 34/day 090/homework/work2.js
@@ -1,13 +1,9 @@
-function manualSort(array) {
-    for (let i = 0; i < array.length - 1; i++) {
-        for (let j = 0; j < array.length - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                let temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
+function filterNumbers(arr, int) {
+  var result = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > int) {
+      result.push(arr[i]);
     }
-    return array;
+  }
+  return result;
 }
-console.log(manualSort([2,10,20,3]))
